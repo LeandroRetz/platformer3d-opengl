57eb6f4c18033c7b8baf37d04aaa826eb1f26410
Leandro Retzlaff <leandroetzlafff@gmail.com>
2026-07-01 19:34:14 -0300

    muda a janela

--- main.c
+++ main.c
@@ -12,16 +12,16 @@ int main(int argc, char **argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowPosition(120, 80);
     glutInitWindowSize(1000, 700);
-    glutCreateWindow("Platformer 3D");
+    glutCreateWindow("Rumo Ao hexa 3D");
 
     /* callbacks */
     glutDisplayFunc(gDesenha);
     glutReshapeFunc(gRedimensiona);
     glutKeyboardFunc(gTeclado);
-    glutKeyboardUpFunc(gTeclaSolta);   /* libera teclas seguradas (WASD)     */
+    glutKeyboardUpFunc(gTeclaSolta);   
     glutMouseFunc(gMouseBotao);
     glutMotionFunc(gMouseMov);
-    glutIgnoreKeyRepeat(1);            /* evita auto-repeat do teclado        */
+    glutIgnoreKeyRepeat(1);          
 
     gInicializa();
     glutTimerFunc(16, gTimer, 0);
